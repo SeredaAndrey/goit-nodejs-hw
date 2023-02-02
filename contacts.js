5ca2655fd897033e4c7cf5942bc8d1069865655a
Andrii Sereda <a.a.sereda@ukr.net>
2023-02-02 16:49:21 +0200

    qweqweqwe

--- contacts.js
+++ contacts.js
@@ -19,3 +19,36 @@ function removeContact(contactId) {
 function addContact(name, email, phone) {
   // ...твій код
 }
+
+const express = require("express");
+
+const app = express();
+
+const PORT = 8081;
+
+// app.use(express.json());
+// app.get("/", async (req, res) => {
+//   // const manifest = await fs.readFile("./db/contacts.json", "utf8");
+
+//   res.send("get request");
+//   // res.json(manifest);
+// });
+// app.post("/", (req, res) => {
+//   res.send("post request");
+// });
+// app.delete("/", (req, res) => {
+//   res.send("delete request");
+// });
+// app.patch("/", (req, res) => {
+//   res.send("patch request");
+// });
+// app.use((req, res) => {
+//   res.send("middleware request");
+// });
+
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.error("Error at server launch:", err);
+//   }
+//   console.log(`server started at port ${PORT}`);
+// });
